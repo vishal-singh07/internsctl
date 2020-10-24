d64448434921333632c163022c273040ea092728
vishal-singh07 <vs1292959@gmail.com>
2020-10-24 11:58:44 +0530

    Added user create and user list command

--- mybash.sh
+++ mybash.sh
@@ -9,6 +9,16 @@ elif [[ ( $1 = "cpu" ) && ( $2 = "getinfo" ) ]]; then
 	lscpu
 elif [[ ( $1 = "memory" ) && ( $2 = "getinfo" ) ]]; then
 	free
+elif [[ ( $1 = "user" ) && ( $2 = "create" ) ]]; then
+	sudo adduser $3
+elif [[ ( $1 = "user" ) && ( $2 = "list" ) ]]; then
+	{
+		if [[ ( $1 = "user" ) && ( $2 = "list" ) && ( $3 = "--sudo-only" ) ]]; then
+	 		grep '^sudo:.*$' /etc/group | cut -d: -f4
+		else
+			cat /etc/passwd | cut -d: -f1
+		fi
+	}
 else
 	echo "please enter valid arguements"
 fi
