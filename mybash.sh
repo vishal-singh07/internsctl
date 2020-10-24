5850c8157cec16ca1193328c2805e01352093780
vishal-singh07 <vs1292959@gmail.com>
2020-10-24 12:05:01 +0530

    Added help command

--- mybash.sh
+++ mybash.sh
@@ -5,6 +5,8 @@ function internsctl() {
 version="v0.1.0"
 if [ $1 = "--version" ]; then
 	echo "current version is  $version "
+elif [ $1 = "--help" ]; then
+	cat internsctlhelp.txt
 elif [[ ( $1 = "cpu" ) && ( $2 = "getinfo" ) ]]; then
 	lscpu
 elif [[ ( $1 = "memory" ) && ( $2 = "getinfo" ) ]]; then
