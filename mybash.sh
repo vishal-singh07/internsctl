3483d1b3b9ff71e12baf5288aa49512d0cfd648b
vishal-singh07 <vs1292959@gmail.com>
2020-10-24 12:00:29 +0530

    Added file getinfo command

--- mybash.sh
+++ mybash.sh
@@ -19,6 +19,43 @@ elif [[ ( $1 = "user" ) && ( $2 = "list" ) ]]; then
 			cat /etc/passwd | cut -d: -f1
 		fi
 	}
+elif [[ ( $1 = "file" ) && ( $2 = "getinfo" ) ]]; then
+	{
+		if (( $# < 3 )); then
+			echo "Invalid command"
+			return
+		fi
+		if [ ! -f ${@: -1} ]; then
+			echo "This file doesn't exist"
+			return
+		fi
+				case $3 in 
+					"--size" | "-s")
+					  ls -l $4 | cut -d' ' -f5
+					  ;;
+					"--permissions" | "-p")
+					  ls -l $4 | cut -d' ' -f1
+					  ;;
+					"--owner" | "-o")
+					  ls -l $4 | cut -d' ' -f3
+					  ;;
+					"--last-modified" | "-m")
+					  stat -c %y $4
+					  ;;
+					*)
+					owner=`ls -l $3 | cut -d' ' -f3`
+					size=`ls -l $3 | cut -d' ' -f5`
+					permission=`ls -l $3 | cut -d' ' -f1`
+					lastModified=`stat -c %y $3`
+			                 echo "File: " $3
+				       	 echo "Access: $permission"
+					 echo "Size(B): $size"
+	 				 echo "Owner: $owner"
+					 echo "Modify: $lastModified"
+				         ;;
+				esac
+	}
+
 else
 	echo "please enter valid arguements"
 fi
