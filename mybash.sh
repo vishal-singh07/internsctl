4529347df184163468e05bfacb185d168410c439
vishal-singh07 <vs1292959@gmail.com>
2020-10-24 11:56:53 +0530

    Added memory getinfo command

--- mybash.sh
+++ mybash.sh
@@ -7,7 +7,8 @@ if [ $1 = "--version" ]; then
 	echo "current version is  $version "
 elif [[ ( $1 = "cpu" ) && ( $2 = "getinfo" ) ]]; then
 	lscpu
-
+elif [[ ( $1 = "memory" ) && ( $2 = "getinfo" ) ]]; then
+	free
 else
 	echo "please enter valid arguements"
 fi
