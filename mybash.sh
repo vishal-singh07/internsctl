9b36dc6571ad7ce14aefe42224701b72095af7ae
vishal-singh07 <vs1292959@gmail.com>
2020-10-24 11:53:15 +0530

    Added cpu getinfo command

--- mybash.sh
+++ mybash.sh
@@ -5,7 +5,8 @@ function internsctl() {
 version="v0.1.0"
 if [ $1 = "--version" ]; then
 	echo "current version is  $version "
-
+elif [[ ( $1 = "cpu" ) && ( $2 = "getinfo" ) ]]; then
+	lscpu
 
 else
 	echo "please enter valid arguements"
